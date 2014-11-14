fa4a9f5932b774328e7d50f08e3ca43acc909fff
Grohden <gabriel_rodhem@hotmail.com>
2014-11-14 16:29:46 -0200

    Delete convertStringToInt.java

--- Java/Android/Tutorials/Conversions/convertStringToInt.java
+++
@@ -1,8 +0,0 @@
-private int[] convertStringToInt(String[] arrayString){
-			int[] ArrayInt=new int[arrayString.length];
-			try{
-				for (int x=0;x!=arrayString.length;x++){	ArrayInt[x]=Integer.parseInt(arrayString[x]);}
-				} catch(NumberFormatException exeption){
-					Toast.makeText(getApplicationContext(), exeption.getLocalizedMessage()+" isnt a number", Toast.LENGTH_LONG).show();
-					};
-				return ArrayInt;
