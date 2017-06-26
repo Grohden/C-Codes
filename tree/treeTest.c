bbf2b75e07a17d327d268a1bd3b22b6258ee710f
Grohden <gabriel_rohden@outlook.com>
2017-06-26 13:05:31 -0300

    Tree test GUI

--- tree/treeTest.c
+++ tree/treeTest.c
@@ -5,30 +5,65 @@
 #include "../chainedList/chainedList.h"
 #include "tree.h"
 
+ChainedList *mainMenuChain = NULL;
+Node *root;
 
-ChainedList * mainMenuChain = NULL;
-
-
-void main(int argc, int argv[]){
+void menu(){
+    //First we need to set screen size
+	setScreenSize(50 * 2, 40);
 
     //We init the chained list
-	if(mainMenuChain == NULL){
+    if (mainMenuChain == NULL)
+    {
 
-		mainMenuChain = initChain();
+        mainMenuChain = initChain();
+        
+        int rootValue;
+        printf("Digite o numero da raiz: ");
+	    scanf("%d", &rootValue);
+        root = initTree(rootValue);
 
-		//Add the items
-		addToChain(mainMenuChain, (void *) "Adicionar conta a fila");
-		addToChain(mainMenuChain, (void *) "Excluir do conta da fila");
-		addToChain(mainMenuChain, (void *) "Ver dados do primeiro");
-		addToChain(mainMenuChain, (void *) "Sair");
-	}
 
-    Node *root = initTree(getRandomNumber()/100);
-
-    int i = 50;
-    while(i--){
-        addValueToTree(root, getRandomNumber()/100);
+        //Add the items
+        addToChain(mainMenuChain, (void *)"Adicionar numero");
+        addToChain(mainMenuChain, (void *)"Ver caminhamento central");
+        addToChain(mainMenuChain, (void *)"Ver caminhamento pos-fixado");
+        addToChain(mainMenuChain, (void *)"Ver caminhamento pre-fixado");
+        addToChain(mainMenuChain, (void *)"Sair");
     }
 
-    pause();
+    //Draw the seletable list and wait for the response
+    int selected = drawSelectableList(mainMenuChain, true);
+    int newValue;
+
+    switch (selected){
+        case 0:
+           
+            printf("Digite o numero : ");
+            scanf("%d", &newValue);
+            addValueToTree(root,newValue);
+            break;
+        case 1:
+            treeShowCentered(root);
+            pause();
+            break;
+        case 2:
+            treeShowPosFixed(root);
+            pause();    
+            break;
+        case 3:
+            treeShowPreFixed(root);
+            pause();        
+            break;
+        case 4:
+            return;
+    }
+
+    printf("\n");
+    menu();
+}
+
+void main(int argc, int argv[])
+{
+    menu();
 }
